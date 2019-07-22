4450e52d5061578e261cc2dee857285a315553d1
eatthatpie <tomek.pugn@gmail.com>
2019-07-22 21:03:13 +0200

    react boilerplate with styles

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '@/assets/style/style.scss';
+
+function App(props) {
+    return (
+        <div className='container'>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum venenatis libero et elementum. Aliquam erat volutpat. Vestibulum quis consectetur lectus, sed faucibus orci. Nam velit tellus, dapibus ac nisi sed, tincidunt rhoncus nulla. Sed id arcu nec nunc viverra posuere. Nunc aliquam blandit est, sit amet sagittis dolor gravida eu. Nulla ac placerat dolor, in pellentesque leo. Morbi non aliquam mi. Duis elementum volutpat varius. Morbi feugiat libero sodales accumsan efficitur. Praesent convallis mattis venenatis. Vivamus tristique tellus in lacus molestie pulvinar. Ut ac pellentesque nibh, in consectetur elit. Nulla facilisi. Donec eget rhoncus sapien.
+            </p>
+            <p>
+                Fusc at cursus justo. Sed pellentesque ac lacus cursus placerat. Vivamus nec pulvinar sem. Integer libero dolor, consectetur quis suscipit sed, euismod ut nibh. Fusce porta, lectus ut convallis gravida, eros tellus ornare lorem, eget egestas arcu lorem et urna. Ut malesuada pharetra elit sed lacinia. Nulla pretium odio ac orci ornare sodales. Ut sed congue eros. Vestibulum et sem bibendum, vehicula nunc quis, accumsan urna. Curabitur nulla nunc, facilisis scelerisque ante eu, feugiat lobortis lectus. Fusce pharetra ultricies lacus, at luctus enim rhoncus at.
+            </p>
+        </div>
+    );
+}
+
+export default App;
