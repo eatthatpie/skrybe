79bdb8b717d30066f80e96eb95bba426e8fe5f00
eatthatpie <tomek.pugn@gmail.com>
2019-07-27 14:25:49 +0200

    mobile version tweaks

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -3,6 +3,7 @@ import Logo from '@/view/logo/Logo';
 import NavContainer from '@/containers/nav/NavContainer';
 import React from 'react';
 import '@/assets/style/style.scss';
+import './App.scss';
 
 function App() {
     return (
