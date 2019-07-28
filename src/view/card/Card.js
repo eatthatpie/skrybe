66ee3988156609ba696889f42f90b93fd79c2548
eatthatpie <tomek.pugn@gmail.com>
2019-07-28 14:20:53 +0200

    editable card fields

--- src/view/card/Card.js
+++ src/view/card/Card.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Card.scss';
 
-function Card(props) {
+function Card() {
     return (
-        <div
-            className="card bg-light"
-            contentEditable="true"
-        />
+        <div className="card bg-light">
+            <div className="card-lead color-light">
+                <div contentEditable="true" />
+                <i class="fas fa-pencil-alt fs-10 color-light bg-light" />
+            </div>
+            <div className="card-body">
+                <div contentEditable="true" />
+                <i className="fas fa-pencil-alt fs-10 color-light bg-light" />
+            </div>
+        </div>
     );
 }
 
