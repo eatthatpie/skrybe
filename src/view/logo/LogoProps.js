ea3f3125d03f7e65955f344cdfe336a3d57f34bd
eatthatpie <tomek.pugn@gmail.com>
2019-09-21 18:29:45 +0200

    update logo

--- src/view/logo/LogoProps.js
+++ src/view/logo/LogoProps.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './LogoProps.scss';
 
-function Logo() {
+function Logo(props) {
     return (
-        <div className="logo-props">
+        <div className={`logo-props ${props.className}`}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 500">
                 <path
                     d="M245 160 L110 360"
