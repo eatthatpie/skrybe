7192313e8323dc1f22ac8942b6af4606b38282ee
eatthatpie <tomek.pugn@gmail.com>
2019-11-25 09:29:46 +0100

    add helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,11 @@
+export const nextTick = function(callback) {
+    setTimeout(callback, Math.floor(0));
+}
+
+export const objectMerge = function() {
+    return Object.assign({}, ...arguments);
+}
+
 export const rand = function(begin, end) {
     return begin + Math.round(Math.random() * (end - begin));
 }
